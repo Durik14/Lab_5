1afa33b99a6918a1160b7512bcefcefe780b4f0e
Durik14 <106884028+Durik14@users.noreply.github.com>
2023-01-30 08:41:36 +0100

    Add files via upload

--- Lab_5/src/Mobile5.java
+++ Lab_5/src/Mobile5.java
@@ -6,7 +6,7 @@ public class Mobile5 extends Warning{
     public Mobile4 mobile4;
 
     public Mobile5(Mobile4 mobile4) {
-        this.flowers4 = flowers4;
+        this.mobile4=mobile4;
     }
 
     @Override
